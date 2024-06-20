08b0d72c42e8e9b24e82fad11bf6972d9bc0dc32
wayo848yowa <142649823+wayo848yowa@users.noreply.github.com>
2024-06-20 07:15:27 +0000

    chap.13　ちゃんと動かない　悲しいね　また家でやろうね

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const pg = require('pg');
 
-let todos = [];
-
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'ToDo App',
-    todos: todos,
+const connection = new pg.Pool({
+  host: 'localhost',
+  user: 'root',
+  password: 'postgres',
+  database: 'todo_app',
+  port: 5432,
   });
-});
+  
+  router.get('/', function (req, res, next) {
+    connection.query(
+      `select * from tasks;`,
+      (error, results) => {
+        console.log(error);
+        console.log(results);
+        res.render('index', {
+          title: 'ToDo App',
+          todos: results,
+        });
+      }
+    );
+  });
 
 router.post('/', function (req, res, next) {
+  connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return
+    }
+    console.log('success');
+  });
   const todo = req.body.add;
-  todos.push(todo);
-  res.redirect('/');
+  connection.query(
+    `insert into tasks (user_id, content) values (1, '${todo}');`,
+    (error, results) => {
+      console.log(error);
+      res.redirect('/');
+    }
+  );
 });
 
 module.exports = router;
