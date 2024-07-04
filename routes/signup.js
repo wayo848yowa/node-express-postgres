99e00df59684355d54f1c89d0a4dd05595341bb9
wayo848yowa <142649823+wayo848yowa@users.noreply.github.com>
2024-07-04 07:09:34 +0000

    chap.15　7/4授業分

--- routes/signup.js
+++ routes/signup.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
@@ -10,29 +9,47 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-    const username = req.body.username;
-    const password = req.body.password;
-    const repassword = req.body.repassword;
-  
-    knex("users")
-      .where({name: username})
-      .select("*")
-      .then(function (result) {
-        if (result.length !== 0) {
-          res.render("signup", {
-            title: "Sign up",
-            errorMessage: ["このユーザ名は既に使われています"],
-          })
-        }
-        console.log(result);
-      })
-      .catch(function (err) {
-        console.error(err);
+  const username = req.body.username;
+  const password = req.body.password;
+  const repassword = req.body.repassword;
+
+  knex("users")
+    .where({name: username})
+    .select("*")
+    .then(function (result) {
+      console.log(result);
+      if (result.length !== 0) {
         res.render("signup", {
           title: "Sign up",
-          errorMessage: [err.sqlMessage],
+          errorMessage: ["このユーザ名は既に使われています"],
+        }) 
+      } else if (password === repassword) {
+        knex("users")
+          .insert({name: username, password: password})
+          .then(function () {
+            res.redirect("/");
+          })
+          .catch(function (err) {
+            console.error(err);
+            res.render("signup", {
+              title: "Sign up",
+              errorMessage: [err.sqlMessage],
+            });
+          });
+      } else {
+        res.render("signup", {
+          title: "Sign up",
+          errorMessage: ["パスワードが一致しません"],
         });
+      }
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render("signup", {
+        title: "Sign up",
+        errorMessage: [err.sqlMessage],
       });
-  });
+    });
+});
 
 module.exports = router;
