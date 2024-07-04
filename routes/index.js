d40f85a8f1a6559cc8feceefa86d09b80f1c56a3
wayo848yowa <142649823+wayo848yowa@users.noreply.github.com>
2024-07-04 06:19:39 +0000

    chap.15の途中まで  動いた～～～

--- routes/index.js
+++ routes/index.js
@@ -1,68 +1,41 @@
+
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
-const pg = require('pg');
 
-const connection = new pg.Pool({
-  host: 'localhost',
-  user: 'root',
-  password: 'postgres',
-  database: 'todo_app',
-  port: 5432,
-  });
-
-  /*新版 */
-  router.get('/', function (req, res, next) {
-    knex("tasks")
-      .select("*")
-      .then(function (results) {
-        console.log(results);
-        res.render('index', {
-          title: 'ToDo App',
-          todos: results,
-        });
-      })
-      .catch(function (err) {
-        console.error(err);
-        res.render('index', {
-          title: 'ToDo App',
-        });
+router.get('/', function (req, res, next) {
+  knex("tasks")
+    .select("*")
+    .then(function (results) {
+      console.log(results);
+      res.render('index', {
+        title: 'ToDo App',
+        todos: results,
       });
-  });
-  
-  /*  旧版
-  router.get('/', function (req, res, next) {
-    connection.query(
-      `select * from tasks;`,
-      (error, results) => {
-        console.log(error);
-        console.log(results.rows);
-        res.render('index', {
-          title: 'ToDo App',
-          todos: results.rows,
-        });
-      }
-    );
-  });
-   */
-
-  router.post('/', function (req, res, next) {
-    const todo = req.body.add;
-    knex("tasks")
-      .insert({user_id: 1, content: todo})
-      .then(function () {
-        res.redirect('/')
-      })
-      .catch(function (err) {
-        console.error(err);
-        res.render('index', {
-          title: 'ToDo App',
-        });
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
       });
-  });
+    });
+});
 
-  
-router.use('/signup',require('./signup'));
+router.post('/', function (req, res, next) {
+  const todo = req.body.add;
+  knex("tasks")
+    .insert({user_id: 1, content: todo})
+    .then(function () {
+      res.redirect('/')
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+      });
+    });
+});
 
+router.use('/signup', require('./signup'));
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -1,10 +1,38 @@
+
 const express = require('express');
 const router = express.Router();
+const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
   res.render('signup', {
     title: 'Sign up',
   });
 });
+
+router.post('/', function (req, res, next) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const repassword = req.body.repassword;
+  
+    knex("users")
+      .where({name: username})
+      .select("*")
+      .then(function (result) {
+        if (result.length !== 0) {
+          res.render("signup", {
+            title: "Sign up",
+            errorMessage: ["このユーザ名は既に使われています"],
+          })
+        }
+        console.log(result);
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render("signup", {
+          title: "Sign up",
+          errorMessage: [err.sqlMessage],
+        });
+      });
+  });
 
 module.exports = router;
